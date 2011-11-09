e63fb1f077e3f6bfca983ed65d83fe7bf9bece98
Samuel Mimram <smimram@gmail.com>
2011-11-08 19:11:57 -0500

    Another test for record polymorphism.
    --HG--
    branch : LS-592

--- scripts/tests/type_errors.pl
+++ scripts/tests/type_errors.pl
@@ -39,6 +39,7 @@ correct('def f(r) = ignore (r.a); ignore (r.b) end ignore (f)');
 correct('def f(r) = [r with a = 5] end ignore(f([b=2]))');
 correct('r = [a=0] def r.b = 5 end ignore (r.a)');
 correct('r = [f = fun (r) -> ignore (r.a)] ignore (r.f([a=5,b=2])) ignore(r.f([a=3]))');
+correct('r = [f = fun (x) -> x] ignore(r.f(3)) ignore(r.f("x"))');;
 
 section("BASIC");
 incorrect('[1]==["1"]');
